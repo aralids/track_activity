dc9277138a702fddf51c3c01e69878ee27881b90
Dilara Sarach <dilara.sarach@abv.bg>
2022-07-08 11:44:22 +0200

    Added summarize()

--- track_activity.py
+++ track_activity.py
@@ -1,3 +1,5 @@
+import time
+import schedule
 from datetime import datetime
 
 def track_activity(sum_dict, log_dict):
@@ -17,14 +19,23 @@ def record_activity(current_time, activity):
     log_file.write(current_time + " : " + activity + "\n")
     log_file.close()
 
+def summarize(sum_dict):
+    lines = ["\n"]
+    for key, value in sum_dict.items():
+        value = str(value)
+        lines.append(key + " : " + value + "\n")
+    lines.append("------------------------------------\n\n")
+    with open("log.txt", "a") as log_file:
+        log_file.writelines(lines)
+
 if __name__ == "__main__":
     sum_dict = {}
     log_dict = {}
+    schedule.every().day.at("11:43").do(summarize, sum_dict)
     while 1:
+        schedule.run_pending()
         sum_dict, log_dict = track_activity(sum_dict, log_dict)
         print("\n------------------------------------")
         for key, value in sum_dict.items():
             print(key, ' : ', value)
         print("------------------------------------\n")
-        for key, value in log_dict.items():
-            print(key, ' : ', value)
