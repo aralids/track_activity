02782233d45d370084ded8c8a4f132c05ae85239
Dilara Sarach <dilara.sarach@abv.bg>
2022-07-08 09:29:12 +0200

    Renamed log_dict to sum_dict, added log_dict

--- track_activity.py
+++ track_activity.py
@@ -1,18 +1,23 @@
 from datetime import datetime
 
-def track_activity(log_dict):
+def track_activity(sum_dict, log_dict):
     activity = input("Activity of the Last 10 Minutes: ")
-    if not (activity in list(log_dict.keys())):
-        log_dict[activity] = 10
+    if not (activity in list(sum_dict.keys())):
+        sum_dict[activity] = 10
+        log_dict[datetime.now().strftime("%d/%m/%Y, %H:%M:%S")] = activity
     else:
-        log_dict[activity] += 10
-    return log_dict
+        sum_dict[activity] += 10
+        log_dict[datetime.now().strftime("%d/%m/%Y, %H:%M:%S")] = activity
+    return sum_dict, log_dict
 
 if __name__ == "__main__":
+    sum_dict = {}
     log_dict = {}
     while 1:
-        log_dict = track_activity(log_dict)
+        sum_dict, log_dict = track_activity(sum_dict, log_dict)
         print("\n------------------------------------")
-        for key, value in log_dict.items():
+        for key, value in sum_dict.items():
             print(key, ' : ', value)
         print("------------------------------------\n")
+        for key, value in log_dict.items():
+            print(key, ' : ', value)
