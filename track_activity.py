eb91fe6ccf1960a31d51a0b40be455ff73d63978
Dilara Sarach <dilara.sarach@abv.bg>
2022-07-08 10:54:50 +0200

    Added record_activity()

--- track_activity.py
+++ track_activity.py
@@ -2,13 +2,20 @@ from datetime import datetime
 
 def track_activity(sum_dict, log_dict):
     activity = input("Activity of the Last 10 Minutes: ")
+    current_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
     if not (activity in list(sum_dict.keys())):
         sum_dict[activity] = 10
-        log_dict[datetime.now().strftime("%d/%m/%Y, %H:%M:%S")] = activity
+        log_dict[current_time] = activity
     else:
         sum_dict[activity] += 10
-        log_dict[datetime.now().strftime("%d/%m/%Y, %H:%M:%S")] = activity
+        log_dict[current_time] = activity
+    record_activity(current_time, activity)
     return sum_dict, log_dict
+
+def record_activity(current_time, activity):
+    log_file = open("log.txt", "a")
+    log_file.write(current_time + " : " + activity + "\n")
+    log_file.close()
 
 if __name__ == "__main__":
     sum_dict = {}
